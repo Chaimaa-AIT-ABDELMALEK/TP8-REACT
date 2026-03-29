b395347b28c0949ba2a986b875f011397f6a6ead
Chaimaa-AIT-ABDELMALEK <c.aitabdelmalek0698@uca.ac.ma>
2026-03-29 02:38:07 +0100

    Update App.test.js

--- tp-api/src/App.test.js
+++ tp-api/src/App.test.js
@@ -1,8 +1,24 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Affichage principal de l’application', () => {
+
+  it('doit afficher le titre du TP', () => {
+    render(<App />);
+    
+    const titre = screen.getByText(/consommation d’une api avec react/i);
+    
+    expect(titre).toBeInTheDocument();
+  });
+
+  it('doit afficher les sections articles et utilisateurs', () => {
+    render(<App />);
+    
+    const articles = screen.getByText(/articles chargés avec fetch/i);
+    const users = screen.getByText(/utilisateurs chargés avec axios/i);
+    
+    expect(articles).toBeInTheDocument();
+    expect(users).toBeInTheDocument();
+  });
+
 });
